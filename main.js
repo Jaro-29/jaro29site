0c4a67401355452dada1bcae8dbbe53f02a55f12
Jaro-29 <jackson.roper1@myyahoo.com>
2024-12-12 11:33:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function(main){
   var n = readFloat("Enter your 'n' value (in decimal form): ");
   var k = readInt("Enter your 'k' value (whole numbers only): ");
   var r = readFloat("Enter your 'r' value: ");
-  var theta = readFloat("Enter your theta value as a pi-less fraction: ")
+  var theta = readFloat("Enter your theta value in radians (use 'Math.PI' instead of pi to ensure your theta value is communicated properly. For example, pi/4 would be Math.PI/4): ")
   for (let i = 0; i < k; i++){
     var currentEquation = 1;
     while (currentEquation != k){
@@ -11,9 +11,11 @@ function(main){
       var exp2 = (theta + (2*k*Math.PI));
       var exp3 = (exp1 * exp2);
       console.log("The exponent that corresponds to the " + i + " root is: " + exp3);
-      currentEquation + 1;
+      currentEquation++;
     }
   }
+  console.log("All exponents should be computed. If any are improperly computed, then open an issue on the issue tracker at https://github.com/Jaro-29/jaro29site/issues");
+  console.log("Make sure your issue hasn't already been logged!");
 }
 
 main();
